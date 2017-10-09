72eb840130237b2ff02c15587c840432def0b419
Poobest Jirathitiphuwadon <j.poobest@Poobests-MacBook-Pro.local>
2017-10-09 22:22:13 +0700

    fragment gallery

--- app/src/main/java/com/poobest/com/mockpeoject/dashboard/fragment/GalleryFragment.java
+++ app/src/main/java/com/poobest/com/mockpeoject/dashboard/fragment/GalleryFragment.java
@@ -1,7 +1,7 @@
 package com.poobest.com.mockpeoject.dashboard.fragment;
 
-
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,23 +9,61 @@ import android.view.ViewGroup;
 
 import com.poobest.com.mockpeoject.R;
 
-
 /**
- * A simple {@link Fragment} subclass.
+ * Created by j.poobest on 9/24/2017 AD.
  */
+
 public class GalleryFragment extends Fragment {
 
-
     public GalleryFragment() {
-        // Required empty public constructor
+        super();
     }
 
+    public static GalleryFragment newInstance() {
+        GalleryFragment fragment = new GalleryFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init(savedInstanceState);
+
+        if (savedInstanceState != null)
+            onRestoreInstanceState(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery_dashboard, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_gallery_dashboard, container, false);
+        initInstances(rootView, savedInstanceState);
+        return rootView;
+    }
+
+    @SuppressWarnings("UnusedParameters")
+    private void init(Bundle savedInstanceState) {
+        // Init Fragment level's variable(s) here
+    }
+
+    @SuppressWarnings("UnusedParameters")
+    private void initInstances(View rootView, Bundle savedInstanceState) {
+        // Init 'View' instance(s) with rootView.findViewById here
+        // Note: State of variable initialized here could not be saved
+        //       in onSavedInstanceState
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save Instance (Fragment level's variables) State here
+    }
+
+    @SuppressWarnings("UnusedParameters")
+    private void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Restore Instance (Fragment level's variables) State here
     }
 
 }
