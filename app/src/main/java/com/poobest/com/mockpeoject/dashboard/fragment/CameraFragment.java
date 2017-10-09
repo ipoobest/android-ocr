744809690bc2ad6fb4b9a7e2c462af6804acf002
Poobest Jirathitiphuwadon <j.poobest@Poobests-MacBook-Pro.local>
2017-10-09 22:09:06 +0700

    use camera

--- app/src/main/java/com/poobest/com/mockpeoject/dashboard/fragment/CameraFragment.java
+++ app/src/main/java/com/poobest/com/mockpeoject/dashboard/fragment/CameraFragment.java
@@ -1,31 +1,98 @@
 package com.poobest.com.mockpeoject.dashboard.fragment;
 
-
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.poobest.com.mockpeoject.R;
 
-
 /**
- * A simple {@link Fragment} subclass.
+ * Created by j.poobest on 9/24/2017 AD.
  */
-public class CameraFragment extends Fragment {
 
+public class CameraFragment extends Fragment implements View.OnClickListener {
+
+    ImageView imageView;
+    Button btnCamera;
 
     public CameraFragment() {
-        // Required empty public constructor
+        super();
     }
 
+    public static CameraFragment newInstance() {
+        CameraFragment fragment = new CameraFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init(savedInstanceState);
+
+        if (savedInstanceState != null)
+            onRestoreInstanceState(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera_dashboard, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_camera_dashboard, container, false);
+        initInstances(rootView, savedInstanceState);
+        return rootView;
     }
 
+    @SuppressWarnings("UnusedParameters")
+    private void init(Bundle savedInstanceState) {
+        // Init Fragment level's variable(s) here
+    }
+
+    @SuppressWarnings("UnusedParameters")
+    private void initInstances(View rootView, Bundle savedInstanceState) {
+        // Init 'View' instance(s) with rootView.findViewById here
+        // Note: State of variable initialized here could not be saved
+        //       in onSavedInstanceState
+
+        imageView = rootView.findViewById(R.id.image_view);
+        btnCamera = rootView.findViewById(R.id.btn_camera);
+
+
+        btnCamera.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save Instance (Fragment level's variables) State here
+    }
+
+    @SuppressWarnings("UnusedParameters")
+    private void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Restore Instance (Fragment level's variables) State here
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnCamera){
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, 0);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+    }
 }
