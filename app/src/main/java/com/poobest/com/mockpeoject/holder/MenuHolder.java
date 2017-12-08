c266709b33d7f6cd448a77f22ca7fbf1998a91e2
DESKTOP-IDUP0PR\RUNGNUENG <rungnueng19@hotmail.com>
2017-12-09 01:48:06 +0700

    add data from firebase to recyclerView

--- app/src/main/java/com/poobest/com/mockpeoject/holder/MenuHolder.java
+++ app/src/main/java/com/poobest/com/mockpeoject/holder/MenuHolder.java
@@ -6,44 +6,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.poobest.com.mockpeoject.R;
-import com.poobest.com.mockpeoject.model.ItemClickCallback;
 
 /**
  * Created by j.poobest on 9/24/2017 AD.
  */
 
-public class MenuHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+public class MenuHolder extends RecyclerView.ViewHolder{
 
     public TextView tvName;
     public ImageView imgView;
-    private ItemClickCallback mListener;
 
-    public MenuHolder(View itemView, ItemClickCallback listener) {
+    public MenuHolder(View itemView) {
         super(itemView);
         tvName = itemView.findViewById(R.id.text_name_menu);
         imgView = itemView.findViewById(R.id.image_menu_list);
-        mListener = listener;
-        itemView.setOnClickListener(this);
     }
-
-
-
-    @Override
-    public void onClick(View view) {
-        mListener.onClick(view, getAdapterPosition());
-
-    }
-
-
-//    public MenuHolder(View itemView) {
-//        super(itemView);
-//
-//        tvName = itemView.findViewById(R.id.text_name_menu);
-//        imgView = itemView.findViewById(R.id.image_menu_list);
-//    }
-//
-//    @Override
-//    public void onClick(View view, int position) {
-//
-//    }
 }
--- app/src/main/java/com/poobest/com/mockpeoject/model/dao/MenuListItem.java
+++ app/src/main/java/com/poobest/com/mockpeoject/model/dao/MenuListItem.java
@@ -1,28 +1,57 @@
 package com.poobest.com.mockpeoject.model.dao;
 
 public class MenuListItem {
-    public String nameMenu;
-    int idImgMenu;
 
-
+    public String name, nameThai, description, ingredient, imgUrl, key;
 
     public MenuListItem() {
-
     }
 
-    public String getNameMenu() {
-        return nameMenu;
+    public String getName() {
+        return name;
     }
 
-    public void setNameMenu(String nameMenu) {
-        this.nameMenu = nameMenu;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public int getIdImgMenu() {
-        return idImgMenu;
+    public String getNameThai() {
+        return nameThai;
     }
 
-    public void setIdImgMenu(int idImgMenu) {
-        this.idImgMenu = idImgMenu;
+    public void setNameThai(String nameThai) {
+        this.nameThai = nameThai;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
     }
 }
